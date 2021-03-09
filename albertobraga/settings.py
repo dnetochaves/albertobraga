64a097032b9d8a86f10f7c0fc3cac042aa4cf856
dnetochaves <dnetochaves@gmail.com>
2021-03-09 15:19:33 -0300

    Refactor leyout

--- albertobraga/settings.py
+++ albertobraga/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-'''
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -137,8 +137,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-LOGIN_REDIRECT_URL = '/user-profile/account/'
-LOGOUT_REDIRECT_URL = '/'
+
 
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
@@ -153,7 +152,9 @@ AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+'''
+LOGIN_REDIRECT_URL = '/user-profile/account/'
+LOGOUT_REDIRECT_URL = '/'
 
 MESSAGE_TAGS = {
     constants.DEBUG: 'alert-info',
