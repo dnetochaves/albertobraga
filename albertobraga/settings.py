b9c5347e3203da810862e522db1dc5fbc34395b3
dnetochaves <dnetochaves@gmail.com>
2021-03-09 06:48:48 -0300

    Foto alberto

--- albertobraga/settings.py
+++ albertobraga/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-'''
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
@@ -131,10 +131,10 @@ STATICFILES_DIRS = [
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-
+'''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-'''
+
 
 LOGIN_REDIRECT_URL = '/user-profile/account/'
 LOGOUT_REDIRECT_URL = '/'
